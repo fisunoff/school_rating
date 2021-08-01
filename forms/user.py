6e8876773e90b352411d2601aaca87d2164ef27c
fisunoff <fisunoff@yandex.ru>
2021-08-01 11:45:35 +0300

    Замена продавец на сотрудник в форме регистрации

--- forms/user.py
+++ forms/user.py
@@ -9,7 +9,7 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired()])
-    role = BooleanField('Я продавец')
+    role = BooleanField('Я сотрудник')
     about = TextAreaField("Немного о себе")
     submit = SubmitField('Войти')
 
